486aa7a3825dac5e8925de8bd045c00660f784a5
Marc Duby <mduby@broadinstitute.org>
2020-12-25 23:48:26 -0500

    Blinkt: added side to side

--- RaspberryPi/Hats/Blinkt/simpleLinear.py
+++ RaspberryPi/Hats/Blinkt/simpleLinear.py
@@ -2,7 +2,43 @@
 from blinkt import set_pixel, set_brightness, show, clear
 import time
 
-def side_to_side(blink_number, sleep_time):
+# set the brightness
+set_brightness(0.1)
+
+# methods
+def side_to_side_motion(blink_number, sleep_time, pixel_number=4):
+    """ will rotate light in circular motion """
+    # set direction
+    direction = 1
+
+    # loop
+    for i in range(blink_number):
+        # set the pixel
+        if pixel_number == 7 or pixel_number == 0:
+            direction = direction * -1
+            pixel_number = pixel_number + direction
+
+        # set the colors
+        red = i * 10
+        blue = 255 - red
+        green = 0
+
+        # clear the old pixel
+        clear()
+
+        # set the new pixel
+        set_pixel(pixel_number, red, green, blue)
+        show() 
+
+        # sleep
+        time.sleep(sleep_time)
+
+    # clear at the end of the loop
+    clear()
+    show()
+
+def circular_motion(blink_number, sleep_time):
+    """ will rotate light in circular motion """
     for i in range(blink_number):
         # set the pixel
         pixel_number = i % 8
@@ -27,4 +63,4 @@ def side_to_side(blink_number, sleep_time):
     show()
 
 # call the function
-side_to_side(50, 0.2)
+side_to_side_motion(50, 0.4)
