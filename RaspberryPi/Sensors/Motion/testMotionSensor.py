774c47b11f6a59079d9023f5c53346d50236cd84
Marc Duby <mduby@broadinstitute.org>
2021-01-22 19:23:02 -0500

    motion: added test motion sensor

--- RaspberryPi/Sensors/Motion/testMotionSensor.py
+++ RaspberryPi/Sensors/Motion/testMotionSensor.py
@@ -5,15 +5,22 @@ import time
 
 # assign the sensor
 sensor = MotionSensor(26)
+sleep_time = 2
+
+# log
+print("starting motion sensor checking")
 
 # infinite loop to keep sensor going
 while True:
-    # log
-    print("starting motion sensor checking")
+    # log sleep
+    print("sleeping for {}".format(sleep_time))
 
     # sleep
-    time.sleep(5)
+    time.sleep(sleep_time)
 
+    # log
+    print("sensor awake")
+    
     # trigger the sensor
     sensor.wait_for_motion()
 
